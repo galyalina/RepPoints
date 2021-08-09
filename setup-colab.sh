e19aeca167fc98a92834d0b73170cd595464ebb0
Galya Litkin <glitkin@gmail.com>
2021-08-09 15:40:55 +0200

    Setup colab, update

--- setup-colab.sh
+++ setup-colab.sh
@@ -1,4 +1,3 @@
-pip show torch
 pip uninstall torch
 
 #Uninstall the current CUDA version
@@ -15,29 +14,25 @@ dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 apt-get update
 apt-get install cuda-10-0
-nvcc --version
 
 #Get conda
 wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-Linux-x86_64.sh && bash Miniconda3-py37_4.8.2-Linux-x86_64.sh -bfp /usr/local
 conda update conda -y -q
 source /usr/local/etc/profile.d/conda.sh
-conda init 
+conda init
 conda install -n root _license -y -q
 conda create -n openmmlab python=3.7 -y
 conda activate openmmlab
-
-conda list cudatoolkit
-conda list cudnn
 
 # install pytorch
 conda install pytorch torchvision torchaudio cudatoolkit=10.0 -c pytorch
 
 # install the latest mmcv
-pip install mmcv-full -f https://download.openmmlab.com/mmcv/dist/cu101/torch1.6.0/index.html
+pip install mmcv==0.2.14
 pip list
 
 # install mmdetection
 git clone --recursive https://github.com/galyalina/RepPoints.git
 cd RepPoints
-pip install -r requirements/build.txt
+pip install -r requirements.txt
 bash init.sh

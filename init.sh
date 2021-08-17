a743cf3d07a7ef74102b7a0adfdb656fd8e83102
Galya Litkin <glitkin@gmail.com>
2021-08-17 20:30:54 +0200

    Fix init.sh

--- init.sh
+++ init.sh
@@ -3,11 +3,11 @@
 # download data
 #sh ./download_coco2017.sh
 #
-## copy files
-#cp -r src/reppoints_head/* mmdetection/mmdet/models/anchor_heads/
-#cp -r src/reppoints_detector/* mmdetection/mmdet/models/detectors/
-#cp -r src/reppoints_generator/* mmdetection/mmdet/core/anchor/
-#cp -r src/reppoints_assigner/* mmdetection/mmdet/core/bbox/assigners/
+# copy files
+cp -r src/reppoints_head/* mmdetection/mmdet/models/anchor_heads/
+cp -r src/reppoints_detector/* mmdetection/mmdet/models/detectors/
+cp -r src/reppoints_generator/* mmdetection/mmdet/core/anchor/
+cp -r src/reppoints_assigner/* mmdetection/mmdet/core/bbox/assigners/
 
 # install mmdetection
 cd mmdetection

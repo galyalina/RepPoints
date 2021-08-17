d8ab38b016803a07e9ee205cbd64904433e21589
Galya Litkin <glitkin@gmail.com>
2021-08-17 18:58:38 +0200

    Update setup file

--- setup-colab.sh
+++ setup-colab.sh
@@ -1,19 +1,19 @@
 pip uninstall torch
 
-#Uninstall the current CUDA version
-apt-get --purge remove cuda nvidia* libnvidia-*
-dpkg -l | grep cuda- | awk '{print $2}' | xargs -n1 dpkg --purge
-apt-get remove cuda-*
-apt autoremove
-apt-get update
-
-#Download CUDA 10.0
-wget  --no-clobber https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-#install CUDA kit dpkg
-dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-apt-get update
-apt-get install cuda-10-0
+##Uninstall the current CUDA version
+#apt-get --purge remove cuda nvidia* libnvidia-*
+#dpkg -l | grep cuda- | awk '{print $2}' | xargs -n1 dpkg --purge
+#apt-get remove cuda-*
+#apt autoremove
+#apt-get update
+#
+##Download CUDA 10.0
+#wget  --no-clobber https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+##install CUDA kit dpkg
+#dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
+#sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
+#apt-get update
+#apt-get install cuda-10-0
 
 #Get conda
 wget https://repo.anaconda.com/miniconda/Miniconda3-py37_4.8.2-Linux-x86_64.sh && bash Miniconda3-py37_4.8.2-Linux-x86_64.sh -bfp /usr/local
